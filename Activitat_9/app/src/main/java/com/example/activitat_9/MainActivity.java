9e02199f300648eb142530dce0b5ad71b23ec671
Daniel-Dziura <d.dziura@hotelinking.com>
2020-11-23 00:14:51 +0100

    A9

---
+++ Activitat_9/app/src/main/java/com/example/activitat_9/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.activitat_9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    static final String[] PAISOS = new String[]{
+            "España", "Francia", "Portugal"
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        String[] paisos = getResources().getStringArray(R.array.paisos_array);
+
+        //ArrayAdapter<String> A = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, PAISOS);
+        //setListAdapter(A);
+        ArrayAdapter<String> a = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, paisos);
+        setListAdapter(a);
+
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+
+
+    }
+
+    public void onListItemClick(ListView parent, View v, int position, long id){
+        String pais = ((TextView) v).getText().toString();
+        Toast.makeText(getApplicationContext(), pais, Toast.LENGTH_SHORT).show();
+
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://es.wikipedia.org/wiki/" + pais));
+        startActivity(i);
+    }
+
+}
